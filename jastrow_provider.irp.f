f5e1f8487cdd38f0ed22aa482539032c10c0e24a
Panadestein <rpana92@gmail.com>
2020-12-10 13:55:53 +0100

    Tabulating. Broken

--- jastrow_provider.irp.f
+++ jastrow_provider.irp.f
@@ -7,10 +7,11 @@ BEGIN_PROVIDER [ double precision, jastrow_full ]
 
  print *, "J_ee = ", factor_ee
  print *, "J_en = ", factor_en
- print *, "J_een = ", factor_een
  print *, "J_enn_naive = ", factor_een_naive
- print *, "J_enn_prog = ", factor_een_prog
+ print *, "J_een = ", factor_een
  print *, "J = J_ee + J_en + J_een = ", factor_ee + factor_en + factor_een
+ print *, "J = J_ee + J_en + J_een_naive = ", factor_ee + factor_en + factor_een_naive
+ !print *, "J_enn_prog = ", factor_een_prog
 
  jastrow_full = dexp(factor_ee + factor_en + factor_een)
 
