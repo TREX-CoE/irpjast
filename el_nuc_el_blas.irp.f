0e5700056b9949a1d6e7da4a4decfa89a7d640b3
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2021-01-18 23:51:00 +0100

    Added blas for feen

--- el_nuc_el_blas.irp.f
+++ el_nuc_el_blas.irp.f
@@ -1,56 +1,57 @@
-BEGIN_PROVIDER [ double precision, factor_een ]
+BEGIN_PROVIDER [ double precision, factor_een_blas ]
   implicit none
   BEGIN_DOC
   ! ElectronE-electron-nuclei contribution to Jastrow factor
   !
-  ! 5436.20340250000
+  ! 4124.84239750000
   END_DOC
   integer                        :: i, j, a, p, k, l, lmax, m, n
-  double precision               :: cn, accu2, accu
+  double precision               :: cn(nnuc), accu
   double precision               :: f(nnuc,0:ncord-2,0:ncord-2)
   double precision               :: tmp_c(nelec,nnuc,0:ncord,0:ncord-1)
 
-  factor_een = factor_een_blas
-  return
+ factor_een_blas = 0.0d0
 
-
- factor_een = 0.0d0
+ ! r_{ij}^k . R_{ja}^l -> tmp_c_{ia}^{kl}
+ do k=0,ncord-1
+   call dgemm('N','N', nelec, nnuc*(ncord+1), nelec, 1.d0,           &
+       rescale_een_e(1,1,k), size(rescale_een_e,1),                  &
+       rescale_een_n(1,1,0), size(rescale_een_n,1), 0.d0,            &
+       tmp_c(1,1,0,k), size(tmp_c,1))
+ enddo
 
  do p = 2, ncord
-    do k = 0, p - 1
-       if (k /= 0) then
-          lmax = p - k
-       else
-          lmax = p - k - 2
-       endif
-       do l = 0, lmax
-          if ( iand(p - k - l, 1) == 1) cycle
+   do k = 0, p - 1
+     m = p-k
+     if (k > 0) then
+       lmax = m
+     else
+       lmax = m - 2
+     endif
 
-          m = (p - k - l) / 2
+     n = shiftr(m,1)
+     do l = iand(m, 1), lmax, 2
 
-          do a = 1, nnuc
-             accu2 = 0.d0
-             cn = cord_vect_lkp(l, k, p, typenuc_arr(a))
-             do j = 2, nelec
-                accu = 0.d0
-                do i = 1, j-1
-                   accu = accu + rescale_een_e(i,j,k) * &
-                    rescale_een_n(i,a,m) * rescale_een_n(j,a,m+l) + &
-                    rescale_een_e(i,j,k) * &
-                    rescale_een_n(i,a,m+l) * rescale_een_n(j,a,m)
-                enddo
-                accu2 = accu2 + accu
-             enddo
-             factor_een = factor_een + accu2 * cn
-          enddo
-
+       do a = 1, nnuc
+         cn(a) = cord_vect_lkp(l, k, p, typenuc_arr(a))
        enddo
-    enddo
+
+       do a = 1, nnuc
+         accu = 0.d0
+         do i=1,nelec
+           accu = accu + rescale_een_n(i,a,n) * tmp_c(i,a,n+l,k)
+         enddo
+         factor_een_blas = factor_een_blas + accu * cn(a)
+       enddo
+       n = n-1
+
+     enddo
+   enddo
  enddo
 
 END_PROVIDER
 
-BEGIN_PROVIDER [ double precision, factor_een_deriv_e, (4, nelec) ]
+BEGIN_PROVIDER [ double precision, factor_een_deriv_e_blas, (4, nelec) ]
  implicit none
  BEGIN_DOC
  ! Dimensions 1-3 : dx, dy, dz
@@ -61,7 +62,7 @@ BEGIN_PROVIDER [ double precision, factor_een_deriv_e, (4, nelec) ]
  double precision, dimension(4) :: driam, drjam_cn, drial, drjal, drijk
  double precision :: cn, v1, v2, d1, d2, lap1, lap2
 
- factor_een_deriv_e = 0.0d0
+ factor_een_deriv_e_blas = 0.0d0
 
  do p = 2, ncord
     do k = 0 , p - 1
@@ -106,14 +107,14 @@ BEGIN_PROVIDER [ double precision, factor_een_deriv_e, (4, nelec) ]
                       d2 = drjam_cn(ii) * riam
                       lap1 = lap1 + d1 * d2
                       lap2 = lap2 + drijk(ii) * drjal(ii)
-                      factor_een_deriv_e(ii, j) = factor_een_deriv_e(ii, j) + v1 * d2 + d1 * v2
+                      factor_een_deriv_e_blas(ii, j) = factor_een_deriv_e_blas(ii, j) + v1 * d2 + d1 * v2
                    enddo
 
                    ! v(x) u''(x) + 2 * u'(x) v'(x) + u(x) v''(x)
                    ii = 4
                    d1 = drijk(ii) * rial + rijk * drjal(ii) + lap2 + lap2
                    d2 = drjam_cn(ii) * riam
-                   factor_een_deriv_e(ii, j) = factor_een_deriv_e(ii, j) + v1 * d2 + d1 * v2 + lap1 + lap1
+                   factor_een_deriv_e_blas(ii, j) = factor_een_deriv_e_blas(ii, j) + v1 * d2 + d1 * v2 + lap1 + lap1
 
                 enddo
              enddo
