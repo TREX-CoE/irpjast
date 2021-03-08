c2ba52845289a1454efea7f2edf767314365b1d5
vijay gopal chilkuri <vijay.gopal.c@gmail.com>
2021-03-08 22:49:30 +0100

    Moved j index to position 1.

--- el_nuc_el_blas.irp.f
+++ el_nuc_el_blas.irp.f
@@ -1,5 +1,5 @@
  BEGIN_PROVIDER [ double precision,  tmp_c, (nelec,nnuc,0:ncord,0:ncord-1) ]
-&BEGIN_PROVIDER [ double precision, dtmp_c, (4, nelec,nnuc,0:ncord,0:ncord-1) ]
+&BEGIN_PROVIDER [ double precision, dtmp_c, (nelec,4,nnuc,0:ncord,0:ncord-1) ]
  implicit none
  BEGIN_DOC
  ! Calculate the intermediate buffers
@@ -22,9 +22,9 @@
  ! dr_{ij}^k . R_{ja}^l -> dtmp_c_{ia}^{kl}
  do k=0,ncord-1
    call dgemm('N','N', 4*nelec, nnuc*(ncord+1), nelec, 1.d0,         &
-       rescale_een_e_deriv_e(1,1,1,k), 4*size(rescale_een_e_deriv_e,2),&
+       rescale_een_e_deriv_e(1,1,1,k), 4*size(rescale_een_e_deriv_e,1),&
        rescale_een_n(1,1,0), size(rescale_een_n,1), 0.d0,            &
-       dtmp_c(1,1,1,0,k), 4*size(dtmp_c,2))
+       dtmp_c(1,1,1,0,k), 4*size(dtmp_c,1))
  enddo
 
 
@@ -67,19 +67,19 @@ END_PROVIDER
        accu = accu + rescale_een_n(j,a,m) * tmp_c(j,a,m+l,k)
 
        factor_een_deriv_e_blas(1:4,j) = factor_een_deriv_e_blas(1:4,j) + (&
-           tmp_c(j,a,m,k) * rescale_een_n_deriv_e(1:4,j,a,m+l) +     &
-           dtmp_c(1:4,j,a,m,k) * rescale_een_n(j,a,m+l) +            &
-           dtmp_c(1:4,j,a,m+l,k) * rescale_een_n(j,a,m) +            &
-           tmp_c(j,a,m+l,k)*rescale_een_n_deriv_e(1:4,j,a,m)         &
+           tmp_c(j,a,m,k) * rescale_een_n_deriv_e(j,1:4,a,m+l) +     &
+           dtmp_c(j,1:4,a,m,k) * rescale_een_n(j,a,m+l) +            &
+           dtmp_c(j,1:4,a,m+l,k) * rescale_een_n(j,a,m) +            &
+           tmp_c(j,a,m+l,k)*rescale_een_n_deriv_e(j,1:4,a,m)         &
            ) * cn(a)
 
        factor_een_deriv_e_blas(4,j) = factor_een_deriv_e_blas(4,j) + (&
-           dtmp_c(1,j,a,m  ,k) * rescale_een_n_deriv_e(1,j,a,m+l) +  &
-           dtmp_c(2,j,a,m  ,k) * rescale_een_n_deriv_e(2,j,a,m+l) +  &
-           dtmp_c(3,j,a,m  ,k) * rescale_een_n_deriv_e(3,j,a,m+l) +  &
-           dtmp_c(1,j,a,m+l,k) * rescale_een_n_deriv_e(1,j,a,m  ) +  &
-           dtmp_c(2,j,a,m+l,k) * rescale_een_n_deriv_e(2,j,a,m  ) +  &
-           dtmp_c(3,j,a,m+l,k) * rescale_een_n_deriv_e(3,j,a,m  )    &
+           dtmp_c(j,1,a,m  ,k) * rescale_een_n_deriv_e(j,1,a,m+l) +  &
+           dtmp_c(j,2,a,m  ,k) * rescale_een_n_deriv_e(j,2,a,m+l) +  &
+           dtmp_c(j,3,a,m  ,k) * rescale_een_n_deriv_e(j,3,a,m+l) +  &
+           dtmp_c(j,1,a,m+l,k) * rescale_een_n_deriv_e(j,1,a,m  ) +  &
+           dtmp_c(j,2,a,m+l,k) * rescale_een_n_deriv_e(j,2,a,m  ) +  &
+           dtmp_c(j,3,a,m+l,k) * rescale_een_n_deriv_e(j,3,a,m  )    &
            )*cn(a)*2.d0
 
      enddo
