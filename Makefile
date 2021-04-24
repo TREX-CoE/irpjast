af8705d22c3df880c801f975d865542908a8f66b
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2021-04-24 03:19:00 +0200

    StarPU OK

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 IRPF90 = irpf90/bin/irpf90 --codelet=factor_een:2 --align=4096 # -s nelec_8:504 -s nnuc:100 -s ncord:5 #-a -d
 #FC     = ifort -xCORE-AVX512 -g -mkl=sequential  -qopt-zmm-usage=high 
 FC     = ifort -xCORE-AVX2 -g
-CC     = gcc -fopenmp
+CC     = gcc -fopenmp $(shell pkg-config --cflags starpu-1.3)
 FCFLAGS= -O3 -I .
 NINJA  = ninja
 ARCHIVE = ar crs
@@ -9,7 +9,7 @@ RANLIB = ranlib
 
 SRC= qmckl_blas_f.f90 qmckl_dgemm.c
 OBJ= IRPF90_temp/qmckl_blas_f.o   IRPF90_temp/qmckl_dgemm.o
-LIB= -mkl=sequential -lgomp
+LIB= -mkl=sequential -lgomp $(shell pkg-config --libs starpu-1.3)
 
 -include irpf90.make
 export
--- qmckl_dgemm.c
+++ qmckl_dgemm.c
@@ -1,9 +1,12 @@
 /* Generated from qmckl_dgemm.org */
 
+#include <starpu.h>
+
 #include <cblas.h>
 #include <stdint.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 
 
@@ -24,70 +27,36 @@ struct dgemm_args {
 };
 
 
-#define MIN_SIZE 512
+void qmckl_dgemm_cl(struct dgemm_args args, double* A, double* B, double* C);
+
+void dgemm_codelet(void *buffers[], void* cl_arg)
+{
+  struct dgemm_args *args = cl_arg;
+  double* A = (double*) STARPU_MATRIX_GET_PTR(buffers[0]);
+  double* B = (double*) STARPU_MATRIX_GET_PTR(buffers[1]);
+  double* C = (double*) STARPU_MATRIX_GET_PTR(buffers[2]);
+  qmckl_dgemm_cl(*args, A, B, C);
+  free(args);
+}
+
+struct starpu_codelet dgemm_cl =
+  {
+   .where = STARPU_CPU,
+   .cpu_funcs = { dgemm_codelet },
+   .cpu_funcs_name = { "dgemm_codelet" },
+   .nbuffers = 3,
+   .max_parallelism = 1,
+   .modes = {STARPU_R, STARPU_R, STARPU_RW},
+
+  };
+
 #include<stdio.h>
 
-static void qmckl_dgemm_rec(struct dgemm_args args) {
-
-//  printf("%5d %5d\n", args.m, args.n);
-
-  if ( (args.m <= MIN_SIZE) || (args.n <= MIN_SIZE)) {
-    #pragma omp task
-    {
-      printf("BLAS %5d %5d %5d\n", args.m, args.n, args.k);
-      cblas_dgemm(CblasColMajor, args.transa, args.transb,
-                  args.m, args.n, args.k, args.alpha,
-                  args.A, args.lda, args.B, args.ldb,
-                  args.beta, args.C, args.ldc);
-    }
-  } else {
-
-    int m1 = args.m / 2;
-    int m2 = args.m - m1;
-    int n1 = args.n / 2;
-    int n2 = args.n - n1;
-      
-    #pragma omp task
-    {
-      struct dgemm_args args_1 = args;
-      args_1.m = m1;
-      args_1.n = n1;
-      qmckl_dgemm_rec(args_1);
-    }
-    
-    #pragma omp task
-    {
-      // TODO: assuming 'N', 'N' here
-      struct dgemm_args args_2 = args;
-      args_2.B = args.B + args.ldb*n1;
-      args_2.C = args.C + args.ldc*n1;
-      args_2.m = m1;
-      args_2.n = n2;
-      qmckl_dgemm_rec(args_2);
-    }
-    
-    #pragma omp task
-    {
-      struct dgemm_args args_3 = args;
-      args_3.A = args.A + m1;
-      args_3.C = args.C + m1;
-      args_3.m = m2;
-      args_3.n = n1;
-      qmckl_dgemm_rec(args_3);
-    }
-    
-    #pragma omp task
-    {
-      struct dgemm_args args_4 = args;
-      args_4.A = args.A + m1;
-      args_4.B = args.B + args.ldb*n1;
-      args_4.C = args.C + m1 + args.ldc*n1;
-      args_4.m = m2;
-      args_4.n = n2;
-      qmckl_dgemm_rec(args_4);
-    }
-  }
-
+void qmckl_dgemm_cl(struct dgemm_args args, double* A, double* B, double* C) {
+    cblas_dgemm(CblasColMajor, args.transa, args.transb,
+                args.m, args.n, args.k, args.alpha,
+                A, args.lda, B, args.ldb,
+                args.beta, C, args.ldc);
 }
 
 void qmckl_dgemm(char transa, char transb,
@@ -132,15 +101,52 @@ void qmckl_dgemm(char transa, char transb,
 
 void qmckl_tasks_run(struct dgemm_args** gemms, int ngemms)
 {
-  #pragma omp parallel
-  {
-    #pragma omp single
+  starpu_init(NULL);
+  
+  starpu_data_handle_t matrix_handle[ngemms][3];
+  for (int i=0 ; i<ngemms ; ++i)
     {
-      for (int i=0 ; i<ngemms ; ++i)
-      {
-        qmckl_dgemm_rec(*(gemms[i]));
-      }
+      starpu_matrix_data_register(&(matrix_handle[i][0]),
+                                    STARPU_MAIN_RAM,
+                                    (uintptr_t) gemms[i]->A,
+                                    gemms[i]->lda,
+                                    gemms[i]->k,
+                                    gemms[i]->m,
+                                    sizeof(double));
+
+      starpu_matrix_data_register(&(matrix_handle[i][1]),
+                                    STARPU_MAIN_RAM,
+                                    (uintptr_t) gemms[i]->B,
+                                    gemms[i]->ldb,
+                                    gemms[i]->n,
+                                    gemms[i]->k,
+                                    sizeof(double));
+
+      starpu_matrix_data_register(&(matrix_handle[i][2]),
+                                    STARPU_MAIN_RAM,
+                                    (uintptr_t) gemms[i]->C,
+                                    gemms[i]->ldc,
+                                    gemms[i]->n,
+                                    gemms[i]->m,
+                                    sizeof(double));
+
+      struct starpu_task *task = starpu_task_create();
+
+      task->cl = &dgemm_cl;
+      task->cl_arg = gemms[i];
+      task->cl_arg_size = sizeof(*gemms[0]);
+      task->handles[0] = matrix_handle[i][0];
+      task->handles[1] = matrix_handle[i][1];
+      task->handles[2] = matrix_handle[i][2];
+      starpu_task_submit(task);
     }
-    #pragma omp taskwait
-  }
+  starpu_task_wait_for_all();
+
+  for (int i=0 ; i<ngemms ; ++i)
+    {
+      starpu_data_unregister(matrix_handle[i][0]);
+      starpu_data_unregister(matrix_handle[i][1]);
+      starpu_data_unregister(matrix_handle[i][2]);
+    }
+  starpu_shutdown();
 }
